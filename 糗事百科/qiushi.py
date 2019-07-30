15f6648923346233667f5512b35cc3c33ed6d7b0
paidaking <43693382+paidaking@users.noreply.github.com>
2019-07-30 10:46:23 +0800

    Update and rename qiushibaike.py to 糗事百科/qiushi.py

--- 糗事百科/qiushi.py
+++ 糗事百科/qiushi.py
@@ -10,7 +10,7 @@ def get_page(url):
     headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.108 Safari/537.36'}
     req = requests.get(url,headers=headers)
     print(req.encoding)
-    return req.text
+    return req.content.decode()
 
 def parse_page(html):
     titles = re.findall('<h2>(.*?)</h2>',html,re.S)
