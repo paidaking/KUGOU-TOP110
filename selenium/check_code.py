a2ec8822602eddd4455f2ccfdf2e0c4752410fbc
paidaking <43693382+paidaking@users.noreply.github.com>
2019-08-22 11:05:18 +0800

    Update and rename 验证码.py to check_code.py

--- selenium/check_code.py
+++ selenium/check_code.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 from PIL import Image, ImageEnhance
+import tesserocr
 import time
 
 
@@ -8,6 +9,7 @@ driver = webdriver.Chrome()
 driver.maximize_window()  # 将浏览器最大化
 driver.get(url)
 time.sleep(1)
+
 # 截取当前网页并命名为printscreen，该网页有我们需要的验证码
 driver.save_screenshot('printscreen.png')
 imgelement = driver.find_element_by_id('txtcheckcode')  # 定位验证码
@@ -15,14 +17,28 @@ location = imgelement.location  # 获取验证码x,y轴坐标
 print(location)
 size = imgelement.size  # 获取验证码的长宽
 print(size)
-rangle = (int(location['x']+300), int(location['y']+150), int(location['x'] + size['width']+245),
-          int(location['y'] + size['height']+190))  # 写成我们需要截取的位置坐标(通过增减数字来确定目标位置)
+rangle = (int(location['x']+310), int(location['y']+160), int(location['x'] + size['width']+218),
+          int(location['y'] + size['height']+175))  # 写成我们需要截取的位置坐标
 
 i = Image.open("printscreen.png")  # 打开截图
 frame4 = i.crop(rangle)  # 使用Image的crop函数，从截图中再次截取我们需要的区域
 frame4=frame4.convert('RGB')
 frame4.save('printscreen.jpg') # 保存我们接下来的验证码图片 进行打码
 
+image = Image.open('printscreen.jpg')
+image = image.convert('L')
+threshold = 127
+table = []
+for i in range(256):
+    if i < threshold:
+        table.append(1)
+    else:
+        table.append(0)
+
+image = image.point(table, '1')
+#image.show()
+result = tesserocr.image_to_text(image)
+print(result)
 
 time.sleep(2)
 driver.close()
