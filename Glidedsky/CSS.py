7cf520df979362602470c600d7c7ed532a2c6f16
paidaking <43693382+paidaking@users.noreply.github.com>
2019-09-03 22:26:58 +0800

    Update CSS.py

--- Glidedsky/CSS.py
+++ Glidedsky/CSS.py
@@ -5,7 +5,7 @@ class Spider_CSS:
     def __init__(self):
         self.url = 'http://glidedsky.com/level/web/crawler-css-puzzle-1?page={}'
         self.headers = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36',
-                        'Cookie': '_ga=GA1.2.186470669.1563545221; Hm_lvt_020fbaad6104bcddd1db12d6b78812f6=1563545221,1564666541; _gid=GA1.2.1802999429.1564666541; remember_web_59ba36addc2b2f9401580f014c7f58ea4e30989d=eyJpdiI6ImpGeWxrUUd6S2xIM1RFK292Y1RJOWc9PSIsInZhbHVlIjoiaEptajJTR1grdXQydHlnY3krRDNDdFZZQWpGOVk0N25TbTh5UEJBdEdMdWtcL25YYnB3a0Y5bmRwSktXTDc0TlNva2ZGMlwvWEVlQVQwa2tqTVIwc0U2NDZCXC9pUUhVZWFOckJ2VlhCcHhLK1UyXC9jWlhIaCsxXC9mdEprdkFxK2d0eWdDRUNyTTRqekFPbHJWblN4ZGRNdVkxT3ZkRUhHa2tTT2R2Z3k5Q2tteEU9IiwibWFjIjoiNmVhYjg0ZTc2MWI0YWEwMzhkNTRjODY2ZWI4MWIwYjlkYWY5YTVlNzMzMjQ4MmEwNDE4MWIwMzZkNTg4NWRkNiJ9; XSRF-TOKEN=eyJpdiI6InBPNG5lQUNoM0ZFNlp3eWVtRnRIOUE9PSIsInZhbHVlIjoiXC83anYrZ1wvS0V2UDBzXC9WeVdUeGRcL3N3TThQVFF4MTRvTytkWkpcL3RVaFBaeDhVcjliRHdoZnVPMGFnNkIwSCtBIiwibWFjIjoiODE1YmViZmVkMGY3OWI0M2EyYjkxYmEwMDU3ZDk2NTIwOWRmZWU5YjUwYzI5MGVhMDY5NDBjOWEzOGVlOTg5MSJ9; glidedsky_session=eyJpdiI6IkFlWGM2VEJhTkJDdjBqUkE1TjlFU3c9PSIsInZhbHVlIjoiXC8rbmV0YXdXN3pIdUY5Q0JRRXVXczRhdWhybXQra0ZLZ0t4SGxpOVM1Y1l3b0hqWnJ5WWVUakYrM1NDTkc3MkwiLCJtYWMiOiIzYzk1ZjVjYzEwMjg0NTI4ZDI2NmViMDllNDllYzc0YTAyM2JiMTQ4ZjFjZGYyZTU4NDg0MzhlNDg4YmUwNWU2In0%3D; Hm_lpvt_020fbaad6104bcddd1db12d6b78812f6=1564669490; _gat_gtag_UA_75859356_3=1'
+                        'Cookie': '******'
                         }
         self.totle_num = 0
 
